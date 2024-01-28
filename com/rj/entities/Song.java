7b63429416a542f79f78253f442b20ac26b55c6a
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-01-28 18:06:02 +0530

    Update Song.java

--- com/rj/entities/Song.java
+++ com/rj/entities/Song.java
@@ -1,1 +1,61 @@
+package com.rj.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class Song {
+	//songid, songName, songArtist this actual created table name db
+	
+	@Id
+	@Column(name = "songid")
+	private int id;
+	
+	@Column(name="songName")
+	private String songName;
+	
+	@Column(name="songArtist")
+	private String artist;
+	
+	
+	public Song() {
+		super();
+	}
+  
+	public Song(int id, String songName, String artist) {
+		super();
+		this.id = id;
+		this.songName = songName;
+		this.artist = artist;
+	}
+  
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSongName() {
+		return songName;
+	}
+	public void setSongName(String songName) {
+		this.songName = songName;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	
+	//require for ReadApp program
+	@Override
+	public String toString() {
+		return "Song [id=" + id + ", songName=" + songName + ", artist=" + artist + "]";
+	}
+	
+	
+}
 
