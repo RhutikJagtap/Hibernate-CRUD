91bee8ce69893cc1586eaf58b4c6a9b7f3b91127
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-01-28 20:04:22 +0530

    Update HibernateUtils.java

--- com/rj/util/HibernateUtils.java
+++ com/rj/util/HibernateUtils.java
@@ -1,1 +1,23 @@
+package com.rj.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.rj.entities.Song;
+
+public class HibernateUtils {
+  
+	//method is public so availabele anywhere
+	public static SessionFactory getSessionFactory() {
+		//create Configuration
+    	Configuration configuration=new Configuration();
+    	configuration.configure(); //it will load hibernate.cfg.xml
+    	
+    	configuration.addAnnotatedClass(Song.class);
+    	
+    	//create sessionfactory
+    	SessionFactory sessionFactory=configuration.buildSessionFactory();
+		return sessionFactory;
+	}
+}
 
